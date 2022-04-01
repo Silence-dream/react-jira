3d7df5a5cafe170e3daafba1a300bda1b39b876e
silence <1428482231@qq.com>
2022-04-01 16:58:40 +0800

    💄格式(node): lint

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -3,24 +3,26 @@ import List from "./List.jsx";
 import { useEffect, useState } from "react";
 import { cleanObject } from "../../utils/index.js";
 import qs from "qs";
-const apiUrl = process.env.REACT_APP_API_URL
+const apiUrl = process.env.REACT_APP_API_URL;
 export const ProjectListScreen = () => {
   const [users, setUsers] = useState([]);
   const [params, setParams] = useState({
     name: "",
-    personId: ""
+    personId: "",
   });
   const [list, setList] = useState([]);
   useEffect(() => {
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(params))}`).then(async response => {
-      if (response.ok) {
-        return setList(await response.json());
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(params))}`).then(
+      async (response) => {
+        if (response.ok) {
+          return setList(await response.json());
+        }
       }
-    });
+    );
   }, [params]);
   // 初始化数据
   useEffect(() => {
-    fetch(`${apiUrl}/users`).then(async response => {
+    fetch(`${apiUrl}/users`).then(async (response) => {
       if (response.ok) {
         return setUsers(await response.json());
       }
@@ -29,8 +31,8 @@ export const ProjectListScreen = () => {
 
   return (
     <>
-      <SearchPanel params={params} setParams={setParams} users={users}/>
-      <List users={users} list={list}/>
+      <SearchPanel params={params} setParams={setParams} users={users} />
+      <List users={users} list={list} />
     </>
   );
 };
